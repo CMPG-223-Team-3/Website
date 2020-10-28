d8b35475462f8fe43aab7d878f79ad87c6d3436f
Ninno Hattingh <ninnohattingh.transformit@gmail.com>
2020-10-28 22:03:10 +0100

    fixed connections

--- Website/WaiterOrders.aspx.cs
+++ Website/WaiterOrders.aspx.cs
@@ -1,22 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using MySql.Data.MySqlClient;
-
+using Website.App_Code;
 
 namespace Website
 {
     public partial class WaiterOrders : System.Web.UI.Page
     {
         private MySqlConnection connection;
-        private string server = "cmpg-223-db.ci6pbvbzz3x3.us-west-1.rds.amazonaws.com";
-        private string database = "sql7368973";
-        private string uid = "admin";
-        private string password = "cmpg22310";
-        string connectionString;
 
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -25,32 +21,32 @@ namespace Website
                 this.BindGrid();
             }
 
-            private void BindGrid()
+            
+        }
+
+        private void BindGrid()
+        {
+            DatabaseConnection connection4545 = new DatabaseConnection();
+            MySqlConnection connection = connection4545.getConnection();
+            using (connection)
             {
-                string constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
-                using (MySqlConnection connection = new MySqlConnection(constr))
-                {
-                    using (MySqlCommand cmd = new MySqlCommand("Order_ID, Table_nr, Status, Paid, CashOrCard FROM tblOrders"))
+                connection.Open();
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM ORDER where Order_ID = @NUM;");
+                cmd.Parameters.AddWithValue("@NUM", 1);
+                    using (MySqlDataAdapter sda = new MySqlDataAdapter())
                     {
-                        using (MySqlDataAdapter sda = new SMyqlDataAdapter())
+                        cmd.Connection = connection;
+                        sda.SelectCommand = cmd;
+                        using (DataTable dt = new DataTable())
                         {
-                            cmd.Connection = connection;
-                            sda.SelectCommand = cmd;
-                            using (DataTable dt = new DataTable())
-                            {
-                                sda.Fill(dt);
-                                GridView1.DataSource = dt;
-                                GridView1.DataBind();
-                            }
+                            sda.Fill(dt);
+                            GridView1.DataSource = dt;
+                            GridView1.DataBind();
                         }
                     }
-                }
             }
         }
 
-            protected System.Void GridView1_SelectedIndexChanged(System.Object sender, System.EventArgs e)
-            {
-
-            }
+        
     }
 }
